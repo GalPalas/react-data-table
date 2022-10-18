1bb4549b5fb8dc2ebd2a3be2205d022892d309bb
Gal Palas <galpalas89@gmail.com>
2022-10-18 10:45:46 +0300

    Dynamically render rows based on data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import data from "./data/data.json";
+
 const App = () => {
-  return <div className="App"></div>;
+  const [contacts, setContact] = useState(data);
+
+  return (
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr className="table-dark">
+            <th scope="col">Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id} className="table-primary">
+              <td>{contact.fullName}</td>
+              <td>{contact.address}</td>
+              <td>{contact.phoneNumber}</td>
+              <td>{contact.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
